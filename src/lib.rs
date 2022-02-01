cea89485671c8aace339f40b4d0c24dea090213d
Julian Ganz <neither@nut.email>
2022-02-01 21:00:39 +0100

    Escaped: add a wrapper for escaping items during formatting
    This change introduces the central utility we want to provide through
    this library.

--- src/lib.rs
+++ src/lib.rs
@@ -49,3 +49,73 @@ impl<F: FnMut(char) -> O + Clone, O: Display> Escaper for F {
     }
 }
 
+
+/// Wrapper for escaping items during formatting
+///
+/// This type wraps an item implementing [Display] together with an [Escaper].
+/// When displayed via its own implementation of [Display], the encapsulated
+/// item will be escaped via the [Escaper] during the formatting process.
+///
+/// # Examples
+///
+/// ```
+/// let escaped = rescue_blanket::Escaped::new("foo=\"bar\"", char::escape_default);
+/// assert_eq!(escaped.to_string(), "foo=\\\"bar\\\"");
+/// ```
+#[derive(Copy, Clone, Debug)]
+pub struct Escaped<I: fmt::Display, E: Escaper> {
+    item: I,
+    escaper: E,
+}
+
+impl<I: fmt::Display, E: Escaper> Escaped<I, E> {
+    /// Create a new wrapper for the given item with an [Escaper]
+    pub fn new(item: I, escaper: E) -> Self {
+        Self {item, escaper}
+    }
+
+    /// Create a new wrapper for the given item with a default [Escaper]
+    pub fn new_default(item: I) -> Self where E: Default {
+        Self {item, escaper: Default::default()}
+    }
+}
+
+impl<I: fmt::Display, E: Escaper + Default> From<I> for Escaped<I, E> {
+    fn from(item: I) -> Self {
+        Self::new_default(item)
+    }
+}
+
+impl<I: fmt::Display, E: Escaper> Display for Escaped<I, E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use fmt::Write;
+
+        let mut out = WriteProxy::new(f, self.escaper.clone());
+        write!(out, "{}", self.item)
+    }
+}
+
+
+/// Escaping [fmt::Write] implementation
+struct WriteProxy<'a, 'b, E: Escaper> {
+    formatter: &'a mut fmt::Formatter<'b>,
+    escaper: E,
+}
+
+impl<'a, 'b, E: Escaper> WriteProxy<'a, 'b, E> {
+    /// Create a new proxy
+    fn new(formatter: &'a mut fmt::Formatter<'b>, escaper: E) -> Self {
+        Self {formatter, escaper}
+    }
+}
+
+impl<E: Escaper> fmt::Write for WriteProxy<'_, '_, E> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        s.chars().try_for_each(|c| self.write_char(c))
+    }
+
+    fn write_char(&mut self, c: char) -> fmt::Result {
+        self.escaper.process(c).fmt(self.formatter)
+    }
+}
+
